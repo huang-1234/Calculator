fe861d2df3e7fdf0b306ea86c1b697bd56eada08
hsq <137707531@qq.com>
2021-03-29 12:10:25 +0800

    CalculatorFirst

---
+++ complexCalculator/complexCalculator.js
@@ -0,0 +1,18 @@
+// JavaScript
+let results="";
+function rec(){
+  if (event.srcElement.innerText=="=") {
+      return;
+  }
+    if (event.srcElement.innerText=="归零") {
+      results="";
+      display.innerText="0";
+      return;
+  }
+  results+=event.srcElement.innerText;
+  display.innerText=results;
+}
+function sum(){
+  calresults=eval(results);
+  display.innerText=calresults;
+}
